354bab7d5c9d8009ce00778a52e433a4504f38e3
JuanOsorner <juanpowerlifting100kg@gmail.com>
2025-08-05 15:43:03 -0500

    feat creando-dinamicas

--- BreveCursoPHP/Crontollers/js/Dinamics.js
+++ BreveCursoPHP/Crontollers/js/Dinamics.js
@@ -1,12 +1,14 @@
+// Im creating a dyynamic bettween html and js
 const saveButton = document.getElementById('saveButton');
 saveButton.addEventListener('click',()=>{
     if(document.getElementById('name').value === "") {
-        alert("Por favor, ingrese su nombre.");
+        alert("Por favor, ingrese su nombre");
         return;
     }else{
         const name = document.getElementById('name').value;
         const text = document.getElementById('userName');
         text.classList.add("userNameTag");
         text.textContent = name;
+        return;
     }
 });
